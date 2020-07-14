6ea35f02a67144d05941b73ea9e421c1590903a2
Nick Song <zibo.song@intelsat.com>
2020-07-14 13:48:47 -0400

    add setting.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import sys
 import pygame
+from setting import setting
 
-class main:
-	pygame.init()
+class mainGame:
+	def __init__(self):
+		pygame.init()
+		self.setting = setting()
+		mainScreen = pygame.display.set_mode(size=(self.setting.screenWidth, self.setting.screenHeight))
+
+	def runGame(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+			pygame.display.flip()
+
+# Main Game
+myGame = mainGame()
+myGame.runGame()
---
+++ setting.py
@@ -0,0 +1,6 @@
+class setting:
+    def __init__(self):
+        self.screenWidth = 1200
+        self.screenHeight = 800
+        self.bgColor = (230, 230, 230)
+        self.caption = "My Game"
